dfc8c5730fb3b2d45b82762c3ab1d7837aafcf8b
Jared Black <binxblack@gmail.com>
2013-03-25 21:51:09 -0700

    Finished name PlayerPref, began working on save points
    also added new scene folders for your use

--- medieval_unity/Assets/scripts/Javascript/nameInputMenu.js
+++ medieval_unity/Assets/scripts/Javascript/nameInputMenu.js
@@ -1,17 +1,17 @@
 var textFieldString = "";
-var namePref : String = PlayerPrefs.GetString("Name");
+var namePref : String;
 
 function OnGUI () {
 	GUI.Label (Rect (25, 25, 200, 100), "Welcome to Medieval Adventure!");
 	GUI.Label (Rect (25, 50, 200, 100), "Enter your name in the field below");
 	textFieldString = GUI.TextField (Rect (25, 75, 200, 25), textFieldString);
+	var namePref = PlayerPrefs.GetString("Name");
 	GUI.Label (Rect (25, 100, 200, 100), namePref);
-	onFinishedEditing();
 }
 
 
-function onFinishedEditing () {
-	print(textFieldString);
-	OnGUI();
+function Update () {
+	if(Input.GetButtonDown("Fire1")) {
+		PlayerPrefs.SetString("Name", textFieldString);
+	}
 }
-
